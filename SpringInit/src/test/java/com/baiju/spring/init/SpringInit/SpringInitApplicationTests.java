906a041d6369fa24eb536cfd9f3bd9f6e28452f8
Baiju Achari <baiju.achari@baesystems.com>
2017-10-17 11:17:11 -0400

    added original version

---
+++ SpringInit/src/test/java/com/baiju/spring/init/SpringInit/SpringInitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.baiju.spring.init.SpringInit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringInitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
